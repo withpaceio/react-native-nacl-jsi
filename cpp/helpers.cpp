f61134151a3bdf07579cd28a137487fecfa8e439
Remy Dautriche <remy.dautriche@gmail.com>
2024-05-27 07:39:24 +0900

    feat: implemented helper function to get an ArrayBuffer from JS

--- cpp/helpers.cpp
+++ cpp/helpers.cpp
@@ -10,4 +10,19 @@ namespace react_native_nacl {
 
     return arrayBuffer;
   }
+
+  std::optional<jsi::ArrayBuffer> getArrayBuffer(jsi::Runtime& jsiRuntime, const jsi::Value& value) {
+    if (!value.isObject()) {
+      throw jsi::JSError(jsiRuntime, "nope hehe");
+      return std::nullopt;
+    }
+
+    jsi::Object object = value.asObject(jsiRuntime);
+    if (!object.isArrayBuffer(jsiRuntime)) {
+      throw jsi::JSError(jsiRuntime, "nope");
+      return std::nullopt;
+    }
+
+    return object.getArrayBuffer(jsiRuntime);
+  }
 }
