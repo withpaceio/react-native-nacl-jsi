2e121475ae98afe0ffddf6ecb813651049df1342
Remy Dautriche <remy.dautriche@gmail.com>
2024-05-27 23:03:01 +0900

    feat: signDetached takes and returns Uint8Array

--- cpp/sign.cpp
+++ cpp/sign.cpp
@@ -1,3 +1,5 @@
+#include <optional>
+
 #include "helpers.h"
 #include "sign.h"
 #include "sodium.h"
@@ -30,20 +32,30 @@ namespace react_native_nacl {
       jsi::PropNameID::forAscii(jsiRuntime, "signDetached"),
       2,
       [](jsi::Runtime& jsiRuntime, const jsi::Value& thisValue, const jsi::Value* arguments, size_t count) -> jsi::Value {
-        std::string message_string = arguments[0].asString(jsiRuntime).utf8(jsiRuntime);
-        std::string secret_key_string = arguments[1].asString(jsiRuntime).utf8(jsiRuntime);
-
-        std::vector<uint8_t> secret_key = base64ToBin(jsiRuntime, secret_key_string);
-        if (secret_key.size() != crypto_sign_SECRETKEYBYTES) {
-          throw jsi::JSError(jsiRuntime, "[react-native-nacl-jsi] crypto_sign_detached wrong secret key length");
+        std::optional<jsi::ArrayBuffer> messageOpt = getArrayBuffer(jsiRuntime, arguments[0]);
+        if (!messageOpt.has_value()) {
+          throw jsi::JSError(jsiRuntime, "[react-native-nacl-jsi] signDetached message must be an ArrayBuffer");
         }
+        auto messageData = messageOpt.value().data(jsiRuntime);
+        auto messageSize = messageOpt.value().size(jsiRuntime);
 
-        std::vector<uint8_t> signature(crypto_sign_BYTES);
-        if (crypto_sign_detached(signature.data(), NULL, (uint8_t *)message_string.data(), message_string.size(), secret_key.data()) != 0) {
+        std::optional<jsi::ArrayBuffer> secretKeyOpt = getArrayBuffer(jsiRuntime, arguments[1]);
+        if (!secretKeyOpt.has_value()) {
+          throw jsi::JSError(jsiRuntime, "[react-native-nacl-jsi] signDetached secretKey must be an ArrayBuffer");
+        }
+        if (secretKeyOpt.value().size(jsiRuntime) != crypto_sign_SECRETKEYBYTES) {
+          throw jsi::JSError(jsiRuntime, "[react-native-nacl-jsi] signDetached wrong secret key length");
+        }
+        auto secretKeyData = secretKeyOpt.value().data(jsiRuntime);
+
+        jsi::ArrayBuffer arrayBuffer = getArrayBuffer(jsiRuntime, crypto_sign_BYTES);
+        uint8_t* signature = arrayBuffer.data(jsiRuntime);
+
+        if (crypto_sign_detached(signature, NULL, messageData, messageSize, secretKeyData) != 0) {
           return jsi::Value(nullptr);
         }
 
-        return jsi::String::createFromUtf8(jsiRuntime, binToBase64(signature.data(), signature.size(), sodium_base64_VARIANT_ORIGINAL));
+        return arrayBuffer;
       }
     );
     jsiRuntime.global().setProperty(jsiRuntime, "signDetached", std::move(signDetached));
