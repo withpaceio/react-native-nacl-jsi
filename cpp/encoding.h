03191eaa339b1c7de1b5057d4deb08de147ebecc
Remy Dautriche <remy.dautriche@gmail.com>
2024-05-28 23:46:55 +0900

    feat: encoding and decoding helper functions

---
+++ cpp/encoding.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include <jsi/jsilib.h>
+#include <jsi/jsi.h>
+
+namespace react_native_nacl {
+  void install_encoding(facebook::jsi::Runtime& jsiRuntime);
+}
