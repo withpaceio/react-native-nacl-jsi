4ab9a293edabcdbd9853f548de1167d69ac633da
Remy Dautriche <remy.dautriche@gmail.com>
2024-05-30 19:57:41 +0900

    feat: updated example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,65 +1,78 @@
 import React from 'react';
 
-import { SafeAreaView, ScrollView, StyleSheet, View, Text } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 import {
-  secretboxGenerateKey,
-  secretboxSeal,
-  secretboxOpen,
-  boxGenerateKey,
-  boxSeal,
-  boxOpen,
-  aesGenerateKey,
-  aesEncrypt,
+  ARGON2ID_MEMLIMIT_INTERACTIVE,
+  ARGON2ID_OPSLIMIT_SENSITIVE,
+  ARGON2ID_SALTBYTES,
   aesDecrypt,
+  aesEncrypt,
+  aesGenerateKey,
+  argon2idDeriveKey,
   argon2idHash,
   argon2idVerify,
-  argon2idDeriveKey,
-  ARGON2ID_OPSLIMIT_SENSITIVE,
-  ARGON2ID_MEMLIMIT_INTERACTIVE,
-  ARGON2ID_SALTBYTES,
+  boxGenerateKey,
+  boxOpen,
+  boxSeal,
+  decodeUtf8,
+  encodeBase64,
+  encodeUtf8,
   getRandomBytes,
-  signGenerateKey,
+  secretboxGenerateKey,
+  secretboxOpen,
+  secretboxSeal,
   signDetached,
+  signGenerateKey,
   signVerifyDetached,
   type AesResult,
 } from 'react-native-nacl-jsi';
 
 export default function App() {
   const aesKey = aesGenerateKey();
-  const { encrypted: aesEncryptedMessage, iv } = aesEncrypt(
-    'hello aes!',
-    aesKey
-  ) as AesResult;
-  const aesDecryptedMessage = aesDecrypt(aesEncryptedMessage, aesKey, iv);
+  const aesResult = aesEncrypt(decodeUtf8('hello aes!'), aesKey) as AesResult;
+  const aesDecryptedMessage = aesDecrypt(
+    aesResult.encrypted,
+    aesKey,
+    aesResult.iv
+  ) as Uint8Array;
 
   const secretKey = secretboxGenerateKey();
-  const encryptedMessage = secretboxSeal('hello with secretbox!', secretKey);
-  const decryptedMessage = secretboxOpen(encryptedMessage, secretKey);
+  const messageSecretbox = decodeUtf8('hello with secretbox!');
+  const encryptedMessage = secretboxSeal(messageSecretbox, secretKey);
+  const decryptedMessage = secretboxOpen(
+    encryptedMessage as Uint8Array,
+    secretKey
+  );
 
   const recipientKeyPair = boxGenerateKey();
   const senderKeyPair = boxGenerateKey();
+
+  const message = decodeUtf8('hello with box!');
   const boxEncryptedMessage = boxSeal(
-    'hello with box!',
+    message,
     recipientKeyPair.publicKey,
     senderKeyPair.secretKey
   );
   const boxDecryptedMessage = boxOpen(
-    boxEncryptedMessage,
+    boxEncryptedMessage as Uint8Array,
     senderKeyPair.publicKey,
     recipientKeyPair.secretKey
   );
 
   const password = 'secure_password1234)';
   const hashedPassword = argon2idHash(
-    password,
+    decodeUtf8(password),
     ARGON2ID_OPSLIMIT_SENSITIVE,
     ARGON2ID_MEMLIMIT_INTERACTIVE
   );
-  const isVerified = argon2idVerify(hashedPassword, password);
+  const isVerified = argon2idVerify(
+    hashedPassword as string,
+    decodeUtf8(password)
+  );
 
   const salt = getRandomBytes(ARGON2ID_SALTBYTES);
   const derivedKey = argon2idDeriveKey(
-    password,
+    decodeUtf8(password),
     salt,
     32,
     ARGON2ID_OPSLIMIT_SENSITIVE,
@@ -67,12 +80,12 @@ export default function App() {
   );
 
   const signKeyPair = signGenerateKey();
-  const messageToSign = 'sign this message';
+  const messageToSign = decodeUtf8('sign this message');
   const signature = signDetached(messageToSign, signKeyPair.secretKey);
   const isSignatureVerified = signVerifyDetached(
     messageToSign,
     signKeyPair.publicKey,
-    signature as string
+    signature as Uint8Array
   );
 
   return (
@@ -80,25 +93,34 @@ export default function App() {
       <ScrollView>
         <View style={styles.algorithmContainer}>
           <Text style={styles.algorithmName}>AES256-GCM</Text>
-          <Text>Key: {aesKey}</Text>
+          <Text>Key: {encodeBase64(aesKey)}</Text>
           <Text>---</Text>
-          <Text>cipher text: {aesEncryptedMessage}</Text>
+          <Text>cipher text: {encodeBase64(aesResult.encrypted)}</Text>
           <Text>---</Text>
-          <Text>clear text: {aesDecryptedMessage}</Text>
+          <Text>
+            clear text: {encodeUtf8(aesDecryptedMessage as Uint8Array)}
+          </Text>
         </View>
         <View style={styles.algorithmContainer}>
           <Text style={styles.algorithmName}>SECRET BOX</Text>
-          <Text>secret key: {secretKey}</Text>
+          <Text>secret key: {encodeBase64(secretKey)}</Text>
           <Text>---</Text>
-          <Text>nonce + cipher text: {encryptedMessage}</Text>
+          <Text>
+            nonce + cipher text: {encodeBase64(encryptedMessage as Uint8Array)}
+          </Text>
           <Text>---</Text>
-          <Text>clear text: {decryptedMessage}</Text>
+          <Text>clear text: {encodeUtf8(decryptedMessage as Uint8Array)}</Text>
         </View>
         <View style={styles.algorithmContainer}>
           <Text style={styles.algorithmName}>BOX</Text>
-          <Text>nonce + cipher text: {boxEncryptedMessage}</Text>
+          <Text>
+            nonce + cipher text:{' '}
+            {encodeBase64(boxEncryptedMessage as Uint8Array)}
+          </Text>
           <Text>---</Text>
-          <Text>clear text: {boxDecryptedMessage}</Text>
+          <Text>
+            clear text: {encodeUtf8(boxDecryptedMessage as Uint8Array)}
+          </Text>
         </View>
         <View style={styles.algorithmContainer}>
           <Text style={styles.algorithmName}>Argon2id</Text>
@@ -111,15 +133,15 @@ export default function App() {
         <View style={styles.algorithmContainer}>
           <Text style={styles.algorithmName}>Argon2id key derivation</Text>
           <Text>Key: {password}</Text>
-          <Text>Salt: {salt}</Text>
+          <Text>Salt: {encodeBase64(salt)}</Text>
           <Text>---</Text>
-          <Text>{derivedKey}</Text>
+          <Text>Derived key: {encodeBase64(derivedKey)}</Text>
         </View>
         <View style={styles.algorithmContainer}>
           <Text style={styles.algorithmName}>Signature</Text>
-          <Text>Public key: {signKeyPair.publicKey}</Text>
-          <Text>Secret key: {signKeyPair.secretKey}</Text>
-          <Text>Signature: {signature}</Text>
+          <Text>Public key: {encodeBase64(signKeyPair.publicKey)}</Text>
+          <Text>Secret key: {encodeBase64(signKeyPair.secretKey)}</Text>
+          <Text>Signature: {encodeBase64(signature as Uint8Array)}</Text>
           <Text>Is verified: {isSignatureVerified ? 'true' : 'false'}</Text>
         </View>
       </ScrollView>
